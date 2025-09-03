34e76a867471984bafa9952411e21f8bcfc67f12
Navyaposham <128964220+Navyaposham@users.noreply.github.com>
2025-09-03 10:30:27 +0530

    Delete IdComparator.java

--- IdComparator.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class IdComparator implements Comparator<Process> {
-  @Override
-  public int compare(Process o1, Process o2) {
-    int diff = o1.getId() - o2.getId();
-    return diff;
-  }
-}
